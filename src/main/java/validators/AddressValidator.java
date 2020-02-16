1eb5004c0327dffbc280075b576d6353f3ee3402
Robert Pilczuk <robertpilczuk@gmail.com>
2020-02-16 11:40:47 +0100

    updatet test for cityValidator

--- src/main/java/validators/AddressValidator.java
+++ src/main/java/validators/AddressValidator.java
@@ -2,6 +2,10 @@ package validators;
 
 import customer.Address;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,9 +23,18 @@ public class AddressValidator {
             System.out.println("Miasto nie może być puste");
             return false;
         }
-        String pattern = "^[a-zA-Z ]*$";
-        Pattern p = Pattern.compile(pattern);
-        Matcher m  = p.matcher(address);
-        return m.matches();
+        List<Character> letters = Arrays.asList('q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z',
+                'x','c','v','b','n','m',' ','ą','ć','ś','ę','ł','ń','ż','ź','ó');
+//        char[] chars = "qwertyuiopasdfghjklzxcvbnm ąćśęłńżźó".toCharArray();
+        address = address.toLowerCase();
+        char[] addressChars = address.toCharArray();
+        for (int i = 0; i < addressChars.length; i++) {
+            if (!letters.contains(addressChars[i])){
+                System.out.println("Adres zawiera niedozwolone znaki");
+                return false;
+            }
+
+        }
+        return true;
     }
 }
--- src/test/java/validators/AddressValidatorTest.java
+++ src/test/java/validators/AddressValidatorTest.java
@@ -8,14 +8,53 @@ import static org.junit.Assert.*;
 
 public class AddressValidatorTest {
     @Test
-    public void shouldCheckIfCityIsNull(){
+    public void shouldCheckIfCityIsNull() {
         //given
         String city = null;
         //when
+        Address address = new Address("", city, "1", "11111");
+        boolean result = AddressValidator.validateCity(address.getCity());
+        //then
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void shouldCheckIfCityIsEmpty() {
+        //given
+        String city = "";
+        //when
+        Address address = new Address("", city, "1", "11111");
         boolean result = AddressValidator.validateCity(city);
         //then
         Assert.assertFalse(result);
-
     }
 
+    @Test
+    public void shouldCheckIfCityContainsSpace() {
+        //given
+        String city1 = " ";
+        String city2 = "abc ";
+        //when
+        Address address1 = new Address("", city1, "1", "11111");
+        Address address2 = new Address("", city2, "1", "11111");
+        boolean result1 = AddressValidator.validateCity(city1);
+        boolean result2 = AddressValidator.validateCity(city2);
+        //then
+        Assert.assertFalse(result1);
+        Assert.assertTrue(result2);
+    }
+    @Test
+    public void shouldCheckIfCityIsValid(){
+        //given
+        String city1 = "Lublin";
+        String city2 = "Biała Podlaska";
+        //when
+        Address address1 = new Address("", city1, "1", "11111");
+        Address address2 = new Address("", city2, "1", "11111");
+        boolean result1 = AddressValidator.validateCity(city1);
+        boolean result2 = AddressValidator.validateCity(city2);
+        //then
+        Assert.assertTrue(result1);
+        Assert.assertTrue(result2);
+    }
 }
