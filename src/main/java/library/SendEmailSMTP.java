1b29b58cd2421889051921f0ee261e9420b01c4c
Pawel Sanaluta <pawel.sanaluta@gmail.com>
2020-02-23 10:26:47 +0100

    email sender configured and tests performed

--- src/main/java/library/SendEmailSMTP.java
+++ src/main/java/library/SendEmailSMTP.java
@@ -1,67 +1,42 @@
 package library;
 
-import com.sun.mail.smtp.SMTPTransport;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.util.Date;
 import java.util.Properties;
 
 public class SendEmailSMTP {
 
-    private static final String SMTP_SERVER = "smtp server ";
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+    public static void sendEmail(String emailAddress, String text, String subject) {
 
-    private static final String EMAIL_FROM = "From@gmail.com";
+        final String username = "polskabibliotekanarodowa@gmail.com";
+        final String password = "_Hs_Ji&fr3Rw7#S";
 
-    private static final String EMAIL_TO_CC = "";
-
-    public static void sendEmail(String receiver, String subject, String message) {
-
-        Properties prop = System.getProperties();
-        prop.put("mail.smtp.host", SMTP_SERVER); //optional, defined in SMTPTransport
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
         prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.port", "25"); // default port 25
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
-        Session session = Session.getInstance(prop, null);
-        Message msg = new MimeMessage(session);
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
 
         try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("polskabibliotekanarodowa@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(emailAddress)
+            );
+            message.setSubject(subject);
+            message.setText(text);
 
-            // from
-            msg.setFrom(new InternetAddress(EMAIL_FROM));
-
-            // to
-            msg.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(receiver, false));
-
-            // cc
-            msg.setRecipients(Message.RecipientType.CC,
-                    InternetAddress.parse(EMAIL_TO_CC, false));
-
-            // subject
-            msg.setSubject(subject);
-
-            // content
-            msg.setText(message);
-
-            msg.setSentDate(new Date());
-
-            // Get SMTPTransport
-            SMTPTransport t = (SMTPTransport) session.getTransport("smtp");
-
-            // connect
-            t.connect(SMTP_SERVER, USERNAME, PASSWORD);
-
-            // send
-            t.sendMessage(msg, msg.getAllRecipients());
-
-            System.out.println("Response: " + t.getLastServerResponse());
-
-            t.close();
+            Transport.send(message);
 
         } catch (MessagingException e) {
             e.printStackTrace();
---
+++ src/test/java/library/EmailTest.java
@@ -0,0 +1,14 @@
+package library;
+
+import org.junit.Test;
+
+public class EmailTest {
+
+    @Test
+    public void shouldSendEmailMessage() {
+        //given
+        Library library = Library.getInstance();
+        library.sendEMail("agnieszka.sanaluta@gmail.com", "aggaewg4sdvf34q^&%gdasdg7ad6*&^87tgfsdf", "(*T*&B^R86cbrta8w76rtc3q4876trq93x87X^&8b6r876tcb76t*&^87643t43q9wnuchrmw0q8c987" +
+                "&^8c7b34ncr9873y498c7yn9x*&G(&^TCr8b7wvtr943w84vr983wyvtn9834vg9G*&B*V$&6tv307tv038cyt08n3cy4)PIfasbw34b.[,pmv9w8er09v8w0398u)V{puibmp5wo8um[PVpo8uepbue");
+    }
+}
